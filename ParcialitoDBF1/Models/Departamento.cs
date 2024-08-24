c9b976897ac0bae5369a8ec422e188ab72834e76
Monvall <gersondaniel.monroy@gmail.com>
2024-08-24 12:51:25 -0600

    Commit

---
+++ ParcialitoDBF1/Models/Departamento.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParcialitoDBF1.Models;
+
+public partial class Departamento
+{
+    public string DepaId { get; set; } = null!;
+
+    public string DepaNombre { get; set; } = null!;
+}
